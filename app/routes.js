aba3d6a443e3619b7ebf8a39e9008e8d7737aa8f
Rob Lavender <Robert.lavender@engineering.digital.dwp.gov.uk>
2021-09-29 08:32:02 +0100

    Add home page and orig send message page. Fix routing

--- app/routes.js
+++ app/routes.js
@@ -4,4 +4,8 @@ const radioButtonRedirect = require('radio-button-redirect')
 router.use(radioButtonRedirect)
 // Add your routes here - above the module.exports line
 
+// router.get('/messages', function(req, res) {
+//   res.render('messages/contact-history');
+// });
+
 module.exports = router
